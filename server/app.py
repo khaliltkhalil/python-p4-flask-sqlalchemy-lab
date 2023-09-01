74b7638fbcc72f36e3e0841f6b3cdec65616e1e4
Khalil Khalil <khalil.t.khalil@gmail.com>
2023-09-01 10:35:38 -0500

    write solution

--- server/app.py
+++ server/app.py
@@ -6,29 +6,71 @@ from flask_migrate import Migrate
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return '<h1>Zoo app</h1>'
+    return "<h1>Zoo app</h1>"
 
-@app.route('/animal/<int:id>')
+
+@app.route("/animal/<int:id>")
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter_by(id=id).first()
+    if not animal:
+        response_body = "No Animal Found"
+        return make_response(response_body, 404)
 
-@app.route('/zookeeper/<int:id>')
+    response_body = f"""
+        <ul>ID: {animal.id}</ul>
+        <ul>Name: {animal.name}</ul>
+        <ul>Species: {animal.species}</ul>
+        <ul>Zookeeper: {animal.zookeeper.name}</ul>
+        <ul>Enclosure: {animal.enclosure}</ul>
+    """
+    return make_response(response_body, 200)
+
+
+@app.route("/zookeeper/<int:id>")
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter_by(id=id).first()
+    if not zookeeper:
+        response_body = "No zookeeper Found"
+        return make_response(response_body, 404)
 
-@app.route('/enclosure/<int:id>')
+    response_body = f"""
+        <ul>ID: {zookeeper.id}</ul>
+        <ul>Name: {zookeeper.name}</ul>
+        <ul>Birthday :{zookeeper.birthday}</ul>
+    """
+    for animal in zookeeper.animals:
+        response_body += f"<ul>Animal: {animal}</ul>"
+
+    return make_response(response_body, 200)
+
+
+@app.route("/enclosure/<int:id>")
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter_by(id=id).first()
+    if not enclosure:
+        response_body = "No enclosure Found"
+        return make_response(response_body, 404)
+
+    response_body = f"""
+        <ul>ID: {enclosure.id}</ul>
+        <ul>Environment: {enclosure.environment}</ul>
+        <ul>Open to visitors :{enclosure.open_to_visitors}</ul>
+    """
+    for animal in enclosure.animals:
+        response_body += f"<ul>Animal: {animal}</ul>"
+
+    return make_response(response_body, 200)
 
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,23 +1,49 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
+    __tablename__ = "zookeepers"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    birthday = db.Column(db.Date)
+
+    animals = db.Relationship("Animal", backref="zookeeper")
+
+    def __repr__(self):
+        return f"{self.name}"
+
 
 class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
+    __tablename__ = "enclosures"
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String)
+    open_to_visitors = db.Column(db.Boolean)
+
+    animals = db.Relationship("Animal", backref="enclosure")
+
+    def __repr__(self):
+        return f"{self.environment}"
+
 
 class Animal(db.Model):
-    __tablename__ = 'animals'
+    __tablename__ = "animals"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey("zookeepers.id"))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey("enclosures.id"))
+
+    def __repr__(self):
+        return f"{self.name}"
